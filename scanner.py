345a796d675a7fe17f5b644623b22920f13fb423
George Oikonomou <g.oikonomou@bristol.ac.uk>
2026-02-03 12:40:57 +0000

    Fix logging statement

--- scanner.py
+++ scanner.py
@@ -127,7 +127,7 @@ def push_to_cloud(temperature, grp_id, rssi):
     current_time = time.time()
     delta_from_last_tx = current_time - last_tx_timestamps[grp_id]
     if delta_from_last_tx < args.min_push_interval:
-        logger.warning("Suppressing Push: Last attempt was %u ago" % (delta_from_last_tx,))
+        logger.warning("Suppressing Push: Last attempt was %us ago" % (delta_from_last_tx,))
         return
 
     try:
